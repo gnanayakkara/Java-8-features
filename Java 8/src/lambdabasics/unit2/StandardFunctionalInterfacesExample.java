27dda3b9f802d0d664fbc402a477c534f916aa3d
Gayan Nanayakkara <34508294+gnanayakkara@users.noreply.github.com>
2022-05-24 19:43:58 +0400

    Apply Consumer interface

--- Java 8/src/lambdabasics/unit2/StandardFunctionalInterfacesExample.java
+++ Java 8/src/lambdabasics/unit2/StandardFunctionalInterfacesExample.java
@@ -1,16 +1,15 @@
-package lambdabasics.unit1Exercise;
+package lambdabasics.unit2;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 /*
  * 24 May 2022
  */
 
-public class Unit2ExerciseJava8 {
+public class StandardFunctionalInterfacesExample {
 
 	public static void main(String[] args) {
 
@@ -22,25 +21,25 @@ public class Unit2ExerciseJava8 {
 		Collections.sort(people, (Person p1, Person p2) -> p1.getLastName().compareTo(p2.getLastName()));
 
 		// Step 2 create a method that prints all the elements in the list
-		performConditionally(people, p -> true,p -> System.out.println(p));
+		printConditionally(people, p -> true);
 
 		// Step 3 Create a method that prints all the people that have last name
 		// beginning with C
 		System.out.println("Print all the people with last Name begining with c"); 
-		performConditionally(people, p -> p.getLastName().startsWith("C"),p -> System.out.println(p));
+		printConditionally(people, p -> p.getLastName().startsWith("C"));
 		
 		// Step 4 Create a method that prints all the people that have last name beginning with C
 		System.out.println("Print all the people with First Name begining with c");
 
-		performConditionally(people, p -> p.getFirstName().startsWith("C"),p -> System.out.println(p.getFirstName()));
+		printConditionally(people, p -> p.getFirstName().startsWith("C"));
 		
 	}
 
-	private static void performConditionally(List<Person> people, Predicate<Person> predicate,Consumer<Person> consumer) {
+	private static void printConditionally(List<Person> people, Predicate<Person> predicate) {
 
 		for (Person person : people) {
 			if (predicate.test(person)) {
-				consumer.accept(person);
+				System.out.println(person);
 			}
 		}
 	}
